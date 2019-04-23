01572ddd7d89f2eb5b9f8de13eb33b8b81bb3299
Adriano Marra <adrianomarra90@gmail.com>
2019-04-22 23:16:19 -0500

    Finish the Add Interest for the Bank application

---
+++ project/bank/model/AccountsInterestManager.java
@@ -0,0 +1,18 @@
+package bank.model;
+
+import framework.IAccount;
+import framework.InterestManager;
+
+public class AccountsInterestManager extends InterestManager {
+
+	@Override
+	public void addAccount(IAccount account) {
+		super.addAccount(account);
+	}
+
+	@Override
+	public void notifyAccounts() {
+		for (IAccount account : super.getAccounts())
+			account.addInterest();		
+	}
+}
